bd87403e76b69b288fe3003b6bb3818690391a2d
Edelmiro Anton <anton.edelmiro@gmail.com>
2023-04-24 20:18:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+// import { Box } from '@mui/system';
+import { Box } from "@mui/material";
+import Splash from "./Components/Splash/Splash";
+
+function App() {
+  return (
+    <>
+      <Splash />
+      <Box sx={{ color: "red" }}>tests</Box>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Splash/Splash.jsx
@@ -0,0 +1,8 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+function Splash() {
+  return <Box>soy splashh</Box>;
+}
+
+export default Splash;
