f1a2b8bcf9f5e93609ac9d40752100a410f7d243
Edelmiro Anton <anton.edelmiro@gmail.com>
2023-05-20 14:03:45 -0300

    add abilities

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,24 +4,39 @@ import Modal from "../Modal/Modal";
 function Card() {
   const [name, setName] = useState(null);
   const [img, setImg] = useState(null);
+  const [abilities, setAbilities] = useState([]);
 
-  const URL = `https://pokeapi.co/api/v2/pokemon/${1}/`;
+  const URL = `https://pokeapi.co/api/v2/pokemon/${30}/`;
 
   useEffect(() => {
     const fetchPokemon = async () => {
       const data = await fetch(URL);
       const result = await data.json();
-      // console.log()
+      console.log(result);
       setName(result.name);
       setImg(result.sprites.front_default);
+      setAbilities(result.abilities);
+      // console.log(abilities.map((el) => el.ability.name));
     };
     fetchPokemon();
   }, []);
 
+  console.log(abilities);
   return (
     <>
       <img src={img} alt="pokemon_image" />
-      <div>{name}</div>
+      <h3>{name.toUpperCase()}</h3>
+      <h4>Abilities</h4>
+      <ul>
+        {abilities.map((el) => (
+          <li>
+            {/* Get and Uppercase the first character of the string */}
+            {el.ability.name.charAt(0).toUpperCase()}
+            {/* Add to the first character the rest of the string */}
+            {el.ability.name.slice(1)}
+          </li>
+        ))}
+      </ul>
       <Modal />
     </>
   );
