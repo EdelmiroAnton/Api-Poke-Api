bf306a7ecfd929889169ec2d541643519529c251
Edelmiro Anton <anton.edelmiro@gmail.com>
2023-05-20 13:17:31 -0300

    add fetch functionality

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import Modal from "../Modal/Modal";
 
 function Card() {
+  const [name, setName] = useState(null);
+  const [img, setImg] = useState(null);
+
+  const URL = `https://pokeapi.co/api/v2/pokemon/${1}/`;
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const data = await fetch(URL);
+      const result = await data.json();
+      // console.log()
+      setName(result.name);
+      setImg(result.sprites.front_default);
+    };
+    fetchPokemon();
+  }, []);
+
   return (
     <>
+      <img src={img} alt="pokemon_image" />
+      <div>{name}</div>
       <Modal />
     </>
   );
